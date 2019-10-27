270d538e4a93f76f623c82f154ecfeec3c3568bf
Gaurav Shriwastava <56992403+gauravshriwastav@users.noreply.github.com>
2019-10-27 20:40:47 +0545

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 int main()
 {
-    int arr[5]={1,2,3,4,5};
-    for(int i=1;i<=4;i++)
+    int arr[10]={1,2,3,4,5,6,7,8,9,10};
+    for(int i=1;i<=10;i++)
     {
         if(arr[0]<arr[i])
         {
@@ -16,8 +17,5 @@ int main()
 
     }
     cout<<"greater"<<arr[0];
-
-
-
     return 0;
 }
